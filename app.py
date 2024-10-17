effd75f1047098653937fcff1f22a6bab3a5f547
niyati295 <you@example.com>
2024-10-17 14:05:02 +0530

    model_training

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
-
+from src.mlproject.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 
 if __name__=="__main__":
@@ -17,7 +17,10 @@ try:
     train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
     data_transformation=DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+    ## Model Training
 
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
 except Exception as e:
     logging.info("Custom Exception")
     raise CustomException(e,sys)
